4fd7ed52f2bc39eca8768abac682049301b564c7
mochajisukmawan <ajie.sukmawan@gmail.com>
2021-01-12 11:24:18 +0700

    hehe

---
+++ application/logs/log-2021-01-12.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-01-12 11:20:13 --> Severity: Notice --> Undefined index: HTTPS_HOST C:\xampp7.2\htdocs\foryouerere\application\config\config.php 28
